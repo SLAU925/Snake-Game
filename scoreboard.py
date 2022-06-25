81db2d513c201b251bae7bda3d84dca4c5387588
SW <laushaowei@gmail.com>
2022-06-25 14:45:48 +0800

    High Score Feature Added

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from turtle import Turtle
 
 FONT = ("Arial",15,"normal")
@@ -31,37 +30,3 @@ class Score(Turtle):
         self.goto(0,0)
         self.write("GAME OVER", move=False, align="center",font=("Monospaced", 20, "normal"))
 
-=======
-from turtle import Turtle
-
-FONT = ("Arial",15,"normal")
-ALIGNMENT = "center"
-class Score(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.hideturtle()
-        self.color("white")
-        self.penup()
-        self.goto(0,270)
-        self.score = 0
-        self.high_score = 0
-        self.write(arg=f"Score : {self.score}  High Score: {self.high_score}", move=False,align=ALIGNMENT, font=FONT)
-
-    def update(self):
-        self.clear()
-        self.write(arg=f"Score : {self.score}  High Score: {self.high_score}", move=False,align=ALIGNMENT, font=FONT)
-
-    def reset(self):
-        if(self.score > self.high_score):
-            self.high_score = self.score
-        self.score = 0
-        self.update()
-        self.write(arg=f"Score : {self.score}  High Score: {self.high_score}", move=False,align=ALIGNMENT, font=FONT)
-
-
-    def gameover(self):
-        self.color("red")
-        self.goto(0,0)
-        self.write("GAME OVER", move=False, align="center",font=("Monospaced", 20, "normal"))
-
->>>>>>> b0f802c7afb4ce774eacc61793ac407c48c0a01d
